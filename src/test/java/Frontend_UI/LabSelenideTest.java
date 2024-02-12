571be67dd3c152b96e9178849e90fd8da75e07d5
foxmentor240 <Foxminded@788>
2024-02-12 11:10:33 +0200

    limt test to UI only

--- src/test/java/Frontend_UI/LabSelenideTest.java
+++ src/test/java/Frontend_UI/LabSelenideTest.java
@@ -1,27 +1,17 @@
 package Frontend_UI;
 
-import com.codeborne.selenide.Selectors;
-import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static org.testng.Assert.assertEquals;
 
 public class LabSelenideTest {
-    private static final Logger log = LoggerFactory.getLogger(LabSelenideTest.class);
 
     @Test
     public void LabSelenideTest() {
-        open("https://www.toolsqa.com/automation-practice-form");
-        log.debug( $(".vc_message_box").getText() );
-        log.debug( $("div.wpb_wrapper h1").getText() );
-        $(Selectors.byPartialLinkText("Partial")).click();
-        $(By.name("firstname")).setValue("My First Name");
-        $(By.name("lastname")).setValue("Last Name");
-        $("#continents").selectOptionContainingText("Australia");
-        $("#selenium_commands").selectOptionContainingText("Browser Commands");
-        $("#selenium_commands").selectOptionContainingText("Wait Commands");
+        open("https://www.pravda.com.ua/eng/");
+        assertEquals($x("//a[contains(text(),'Support Us')]").getAttribute("href"),  "https://www.pravda.com.ua/eng/supportus/");
     }
 }
