fcab099c0827a51e192f6aaefd506a213056facf
foxmentor240 <Foxminded@788>
2024-02-12 11:27:54 +0200

    fix https://github.com/SeleniumHQ/selenium/issues/12973 issue

--- src/test/java/Frontend_UI/LabSelenideTest.java
+++ src/test/java/Frontend_UI/LabSelenideTest.java
@@ -1,5 +1,8 @@
 package Frontend_UI;
 
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -11,6 +14,11 @@ public class LabSelenideTest {
 
     @Test
     public void LabSelenideTest() {
+        //chrome_options.add_argument('--remote-debugging-pipe')
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-debugging-pipe");
+        Configuration.browserCapabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         open("https://www.pravda.com.ua/eng/");
         assertEquals($x("//a[contains(text(),'Support Us')]").getAttribute("href"),  "https://www.pravda.com.ua/eng/supportus/");
     }
